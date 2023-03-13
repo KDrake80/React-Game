7660ea27133e25969d63d20cc2a38c16fe6da076
Kevin Drake <94082862+KDrake80@users.noreply.github.com>
2023-03-13 08:45:53 -0600

    Initial Upload
    All the Files I have so far for my Project Presentation

---
+++ Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+        <h1>Welcome to my Game, Please log in. If you don't have an account, Create an account.</h1>
+        </header>
+    )
+}
+
+export default Header
